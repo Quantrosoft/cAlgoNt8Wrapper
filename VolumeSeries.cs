f7ad8d53bafd2eb93cf78671b12be045ec760f9c
Heinrich Munz <Heinrich.Munz@Munz-Endeavors.com>
2025-05-06 15:20:01 +0200

    Added MIT license

--- VolumeSeries.cs
+++ VolumeSeries.cs
@@ -1,17 +1,27 @@
-﻿//
-// Copyright (c) 2035 Quantrosoft Pty. Ltd.
-// All rights reserved.
-//
+﻿/* MIT License
+Copyright (c) 2035 Quantrosoft Pty. Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
 
 using NinjaTrader.Data;
 using NinjaTrader.NinjaScript;
-using SevenZip.Buffer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using TdsCommons;
 
 namespace cAlgo.API
@@ -91,8 +101,8 @@ namespace cAlgo.API
                 if (0 == index)
                     return 0;
                 else
-                    return (double)((long)mNinjaVolumeSeriesArray[mBars.AskBarsIndex][index] << 34
-                        | (long)mNinjaVolumeSeriesArray[mBars.BidBarsIndex][index] << 2);
+                    return (long)mNinjaVolumeSeriesArray[mBars.AskBarsIndex][index] << 34
+                        | (long)mNinjaVolumeSeriesArray[mBars.BidBarsIndex][index] << 2;
             }
         }
 
